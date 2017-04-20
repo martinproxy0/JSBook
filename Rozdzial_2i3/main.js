4d9331f030b087a659e1ebd464cf27149676bfdb
Marcin Sornek <martinproxy0@gmail.com>
2017-04-21 00:17:18 +0200

    rozdzial 3 zadanie 1 - dziala :)

--- Rozdzial_2i3/main.js
+++ Rozdzial_2i3/main.js
@@ -1,4 +1,15 @@
 
 const witaj = document.getElementById('witka')
+const refR3Z1 = document.getElementById('R3Z1')
+
+
 
 witaj.innerHTML = 'hello';
+
+
+function mnoz(a,b) {
+  var c = a * b;
+  return c;
+}
+
+refR3Z1.innerHTML = mnoz(2,2);
